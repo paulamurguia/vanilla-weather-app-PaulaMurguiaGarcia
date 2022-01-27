97554399dad0ded56c2eb831c4b3f5f689479364
Paula Murguia <paula.murguia@gmail.com>
2022-01-26 21:25:37 -0600

    Current temperature added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-alert("hello world");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let units = "imperial";
+let apiKey = "a854789e87fa77e4522cbc1494195a4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
