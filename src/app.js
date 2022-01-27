3a596f461194a43376026df386b95e8c5297a7c6
Paula Murguia <paula.murguia@gmail.com>
2022-01-27 13:11:09 -0600

    Updates to form and added search engine

--- src/app.js
+++ src/app.js
@@ -44,9 +44,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let units = "imperial";
-let city = "New York";
-let apiKey = "a854789e87fa77e4522cbc1494195a4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "a854789e87fa77e4522cbc1494195a4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", handleSubmit);
